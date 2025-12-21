feb1650e6a3119b69a06567467a9b21d5255240b
d3oxy <ishansachu1@gmail.com>
2025-12-21 18:04:42 +0530

    Initial Commit

---
+++ Cloak/CloakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloakApp.swift
+//  Cloak
+//
+//  Created by Ishan on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CloakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
